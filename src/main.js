3ac33d0d23eed8b321fae9cf012bdccfb3a6b9e6
Zack Borges <zackartborges@gmail.com>
2021-04-09 16:25:06 -0400

    adds vue bootstrap login modal. and bootstrap links

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,16 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <div class="login">
-      <button>Sign In</button>
+      <div>
+        <!-- Using modifiers -->
+        <b-button v-b-modal.my-modal>Show Modal</b-button>
+
+        <!-- Using value -->
+        <!-- <b-button v-b-modal="'my-modal'">Show Modal</b-button> -->
+
+        <!-- The modal -->
+        <b-modal id="my-modal">Hello From My Modal!</b-modal>
+      </div>
     </div>
   </div>
 </template>
@@ -10,15 +19,38 @@
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to OurPack",
+      email: "",
+      password: "",
+      errors: [],
     };
   },
   created: function () {},
   methods: {
     login: function () {},
+    submit: function () {
+      var params = {
+        email: this.email,
+        password: this.password,
+      };
+      axios
+        .post("/api/sessions", params)
+        .then((response) => {
+          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        });
+    },
   },
 };
 </script>
