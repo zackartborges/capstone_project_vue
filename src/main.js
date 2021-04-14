30cd555170c46c706155dcf54c7e2ea848209337
Zack Borges <zackartborges@gmail.com>
2021-04-14 14:59:06 -0400

    adds smart table import and vue.use into main.js

--- src/main.js
+++ src/main.js
@@ -3,19 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import SmartTable from "vuejs-smart-table";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
+// Data table for vue with vanilla HTML structure
+Vue.use(SmartTable);
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
-
 
 Vue.config.productionTip = false;
 
