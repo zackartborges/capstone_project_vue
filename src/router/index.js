fd618667a62c9ae3f17d86150c368ecbf7a2900f
Zack Borges <zackartborges@gmail.com>
2021-04-14 10:37:07 -0400

    adds signup page, path and router link

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Signup from "../views/Signup.vue";
+
 
 Vue.use(VueRouter);
 
@@ -15,6 +17,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
   },
   {
     path: "/about",
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,19 +1,63 @@
 <template>
   <div class="signup">
-    <h1>{{ message }}</h1>
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      <div class="form-group">
+        <label>Name:</label>
+        <input type="text" class="form-control" v-model="name" />
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="email" class="form-control" v-model="email" />
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" class="form-control" v-model="password" />
+      </div>
+      <div class="form-group">
+        <label>Password confirmation:</label>
+        <input type="password" class="form-control" v-model="passwordConfirmation" />
+      </div>
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form>
   </div>
 </template>
 
-<style></style>
-
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Sign up here!",
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: [],
     };
   },
-  created: function () {},
-  methods: {},
+  methods: {
+    submit: function () {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation,
+      };
+      axios
+        .post("/api/users", params)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/login");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
 };
 </script>
