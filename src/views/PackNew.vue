f2da76d560a9bd46e7dca648046e712d0bc2cde8
Zack Borges <zackartborges@gmail.com>
2021-04-14 22:42:57 -0400

    adds indexPacks to 'new page'

--- src/views/PackNew.vue
+++ src/views/PackNew.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div class="example">
+      <div v-for="pack in packs" :key="pack.id">
+        <p>{{ pack.id }}</p>
+        Name:
+        <p>{{ pack.gear_name }}</p>
+        Description:
+        <p>{{ pack.gear_description }}</p>
+        Weight:
+        <p>{{ pack.gear_weight }} oz.</p>
+        Quantity:
+        <p>{{ pack.gear_quantity }}</p>
+        <hr />
+        <!-- <p>{{ pack.gear.description }}</p>
+      <p>{{ pack.gear.weight }}</p> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Create your pack here.",
+      packs: [],
+      user: "",
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexPacks();
+  },
+  methods: {
+    indexPacks: function () {
+      axios.get("/api/packs").then((response) => {
+        this.packs = response.data;
+        console.log("all packs:", this.packs);
+      });
+    },
+  },
 };
 </script>
