e2087f30aec0aa5f859d04763546c64d7209f3f0
Zack Borges <zackartborges@gmail.com>
2021-05-07 12:30:30 -0400

    adds oz to lbs

---
+++ src/components/myChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <canvas id="my-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+import myPackData from "../pack-data.js";
+
+export default {
+  name: "myChart",
+  data() {
+    return {
+      myPackData: myPackData,
+    };
+  },
+  mounted() {
+    const ctx = document.getElementById("pack-chart");
+    new Chart(ctx, this.myPackData);
+  },
+};
+</script>
