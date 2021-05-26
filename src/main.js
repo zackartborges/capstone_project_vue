44b0781c35dfa3a85e7fd1ec55a5c770dee3adeb
Zack Borges <zackartborges@gmail.com>
2021-05-26 11:10:25 -0400

    adds ourpack route in main.js

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ Vue.use(IconsPlugin);
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://ourpack.herokuapp.com" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
